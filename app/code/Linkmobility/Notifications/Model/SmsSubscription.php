058983c4939dcef55a5530a727b01c3932984f74
yagator <yagator@gmail.com>
2018-12-10 16:40:58 -0600

    SMS Models contructor

--- app/code/Linkmobility/Notifications/Model/SmsSubscription.php
+++ app/code/Linkmobility/Notifications/Model/SmsSubscription.php
@@ -29,29 +29,6 @@ class SmsSubscription extends AbstractModel
      */
     protected $_eventObject = 'sms_subscription';
 
-
-    /**
-     * @param \Magento\Framework\Model\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Data\FormFactory $formFactory
-     * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate
-     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
-     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
-     * @param array $data
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     */
-    public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Data\FormFactory $formFactory,
-        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        array $data = []
-    ) {
-        parent::__construct($context, $registry, $formFactory, $localeDate, $resource, $resourceCollection, $data);
-    }
-
     /**
      * Set resource model and Id field name
      *
