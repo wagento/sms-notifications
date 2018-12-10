d69f7d526b1027b90e075baf67c3b38d4cc562c1
yagator <yagator@gmail.com>
2018-12-10 15:51:19 -0600

    SMS API Client: sender extends fix

--- app/code/Linkmobility/Notifications/Model/Api/Sms/Send.php
+++ app/code/Linkmobility/Notifications/Model/Api/Sms/Send.php
@@ -1,7 +1,7 @@
 <?php
 namespace Linkmobility\Notifications\Model\Api\Sms;
 
-class Send extends Linkmobility\Notifications\Model\Api\Client {
+class Send extends \Linkmobility\Notifications\Model\Api\Client {
 
     
 }
