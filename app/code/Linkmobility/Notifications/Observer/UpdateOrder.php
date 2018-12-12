70ff001c6978e7d359dc35f55e9ce28615933a94
yagator <yagator@gmail.com>
2018-12-12 02:25:31 -0600

    SMS API update order observers

---
+++ app/code/Linkmobility/Notifications/Observer/UpdateOrder.php
@@ -0,0 +1,47 @@
+<?php
+namespace Linkmobility\Notifications\Observer;
+
+
+class CreateOrder  implements \Magento\Framework\Event\ObserverInterface {
+
+    protected $_logger;
+    protected $_sender;
+    protected $_scopeConfig;
+
+
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Linkmobility\Notifications\Model\Api\Sms\Send $sender
+    ) {
+        $this->_logger = $logger;
+        $this->_scopeConfig = $scopeConfig;
+        $this->_sender = $sender;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+        $event = $observer->getEvent();
+        $order = $event->getOrder();
+        $address = ($order->getShippingAddress() ? : $order->getBillingAddress());
+        $telephone = ($address ? $address->getTelephone() : NULL);
+
+        $this->_sender
+            ->setSource(
+                $this->_scopeConfig->getValue("customer/linkmobility_notifications/source_number")
+            )
+            ->setDestination($telephone)
+            ->setUserData(
+                "Your order number {$order->getIncrementId()} has been updated, please verify in your account. Thank you."
+            );
+        $this->_logger->info("Linkmobility: preparing request");
+        try {
+            $response = $this->_sender->execute();
+            $this->_logger->info("Linkmobility: response received");
+            $this->_logger->info(print_r($response, TRUE));
+        }catch (\Exception $e){
+            $this->_logger->info($e->getMessage());
+        }
+
+        return $this;
+    }
+}
