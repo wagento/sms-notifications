11c0a3d57866c268b8f98e65e70181c6d3318554
Joseph Leedy <joseph@wagento.com>
2018-12-14 09:34:29 -0600

    Remove Magento and move extension to repo root

--- dev/tests/integration/testsuite/Magento/SalesRule/_files/coupons_limited_order_rollback.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-require __DIR__ . '/../../../Magento/Sales/_files/order_rollback.php';
-require 'coupons_limited_rollback.php';
