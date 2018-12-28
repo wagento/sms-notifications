f916ea779779ad3fe8016896922f76487fdfc201
Joseph Leedy <joseph@wagento.com>
2018-12-28 14:22:55 -0600

    Fix SMS type selections for mass actions not submitted (REED-77)
    This is a work-around for a core bug in Magento which is supposed to be
    fixed in 2.2.28 and 2.3.1.

---
+++ view/base/requirejs-config.js
@@ -0,0 +1,20 @@
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/grid/massactions': {
+                'Linkmobility_Notifications/js/grid/massactions-mixin': true
+            }
+        }
+    }
+};
