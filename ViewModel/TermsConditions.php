a8cddc4b77e8b320d0cc5b025baa4c0ecd66b5a1
Joseph Leedy <joseph@wagento.com>
2019-01-16 15:05:04 -0600

    Refactor SMS subscription user interface
    - Renamed template containing subscription fields
    - Removed custom modal used for "Subscription Preferences" pop-up
    - Removed tooltip component used to display "Terms and Conditions"
    content
    - Created UI components for "Terms and Conditions" and "Subscription
    Preferences" modals based on built-in modal component
    - Created View Model for retrieving SMS Notification Types as a grouped
    object
    - Created View Model for retrieving Terms and Conditions content
    - Created Blocks for controlling output of form fields and UI components
    based on whether module is enabled

---
+++ ViewModel/TermsConditions.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @package Linkmobility\Notifications\ViewModel
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+declare(strict_types=1);
+
+namespace Linkmobility\Notifications\ViewModel;
+
+use Linkmobility\Notifications\Api\ConfigInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Terms & Conditions View Model
+ *
+ * @package Linkmobility\Notifications\ViewModel
+ * @author Joseph Leedy <joseph@wagento.com>
+ */
+class TermsConditions implements ArgumentInterface
+{
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+    /**
+     * @var \Linkmobility\Notifications\Api\ConfigInterface
+     */
+    private $config;
+
+    public function __construct(StoreManagerInterface $storeManager, ConfigInterface $config)
+    {
+        $this->storeManager = $storeManager;
+        $this->config = $config;
+    }
+
+    public function getContent(): string
+    {
+        try {
+            $websiteId = (string)$this->storeManager->getStore()->getWebsiteId();
+        } catch (NoSuchEntityException $e) {
+            $websiteId = null;
+        }
+
+        return $this->config->getTermsAndConditions($websiteId) ?? '';
+    }
+}
---
+++ view/frontend/web/js/model/sms-subscription-preferences-modal.js
@@ -0,0 +1,57 @@
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal',
+    'mage/translate'
+], function ($, modal, $t) {
+    'use strict';
+
+    return {
+        modalWindow: null,
+        /**
+         * Create pop-up window for provided element.
+         *
+         * @param {HTMLElement} element
+         * @param {string} title
+         * @param {function(Object)} onClose
+         */
+        createModal: function (element, title, onClose) {
+            this.modalWindow = element;
+
+            const options = {
+                title: title,
+                responsive: true,
+                buttons: [
+                    {
+                        text: $t('Save'),
+                        class: 'action primary',
+                        /** @inheritdoc */
+                        click: function () {
+                            if (onClose && typeof onClose === 'function') {
+                                onClose(this);
+                            }
+                            
+                            this.closeModal();
+                        }
+                    }
+                ]
+            };
+
+            modal(options, $(this.modalWindow));
+        },
+        showModal: function () {
+            $(this.modalWindow).modal('openModal');
+        }
+    };
+});
