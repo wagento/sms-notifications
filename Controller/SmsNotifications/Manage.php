26e798319e67d70e4628e815e5cf04a0d3889562
Joseph Leedy <joseph@wagento.com>
2019-01-23 10:43:26 -0600

    Fix incorrect formatting and add missing headers (REED-74)

--- Controller/SmsNotifications/Manage.php
+++ Controller/SmsNotifications/Manage.php
@@ -1,12 +1,37 @@
 <?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @package Linkmobility\Notifications\Controller\SmsNotifications
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+declare(strict_types=1);
+
 namespace Linkmobility\Notifications\Controller\SmsNotifications;
 
-class Manage extends \Magento\Customer\Controller\AbstractAccount {
+use Magento\Customer\Controller\AbstractAccount;
 
-    public function execute() {
-
+/**
+ * Manage SMS Subscriptions Controller
+ *
+ * @package Linkmobility\Notifications\Controller\SmsNotifications
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @author Joseph Leedy <joseph@wagento.com>
+ */
+class Manage extends AbstractAccount
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
         $this->_view->loadLayout();
         $this->_view->renderLayout();
     }
-
 }
