a87495565777e46a18343e79094662c1ddd7866b
Joseph Leedy <joseph@wagento.com>
2019-01-23 19:20:35 -0600

    Create and use jQuery widget for toggling checkboxes (REED-109)

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,19 @@
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+var config = {
+    map: {
+        '*': {
+            lmCheckboxToggler: 'Linkmobility_Notifications/js/checkbox-toggler'
+        }
+    }
+};
