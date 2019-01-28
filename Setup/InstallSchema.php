5c71dd57f777f9325197de918e80c5491768bb4f
Joseph Leedy <joseph@wagento.com>
2019-01-28 15:48:35 -0600

    Add missing file and class header to installers

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @package Linkmobility\Notifications\Setup
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+declare(strict_types=1);
 
 namespace Linkmobility\Notifications\Setup;
 
@@ -7,6 +21,12 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
 /**
+ * Database Schema Installer
+ *
+ * @package Linkmobility\Notifications\Setup
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @author Joseph Leedy <joseph@wagento.com>
+ *
  * @codeCoverageIgnore
  */
 class InstallSchema implements InstallSchemaInterface
