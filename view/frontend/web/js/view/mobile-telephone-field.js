1db7e292225afa9258ac07f5ce2f458f6755eecf
Joseph Leedy <joseph@wagento.com>
2019-02-07 08:54:51 -0600

    Add missing file header to stylesheet and script (REED-94)

--- view/frontend/web/js/view/mobile-telephone-field.js
+++ view/frontend/web/js/view/mobile-telephone-field.js
@@ -1,3 +1,15 @@
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
 define([
     'ko',
     'uiComponent',
