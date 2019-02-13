a687823966434fe3d15b6b9e63507519d707a47c
Joseph Leedy <joseph@wagento.com>
2019-02-13 15:03:03 -0600

    Fix incorrect file header in order fixtures (REED-126)

--- Test/Integration/_files/order_holded.php
+++ Test/Integration/_files/order_holded.php
@@ -5,7 +5,6 @@
  * Sends transactional SMS notifications through the LINK Mobility messaging
  * service.
  *
- * @package Linkmobility\Notifications\Model
  * @author Joseph Leedy <joseph@wagento.com>
  * @author Yair García Torres <yair.garcia@wagento.com>
  * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
