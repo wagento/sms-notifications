3f0915dc281f486e6b99e4a5b007f7ea6952d024
Joseph Leedy <joseph@wagento.com>
2019-02-15 14:06:54 -0600

    Create plugin to mark order as released from hold (REED-126)

---
+++ Plugin/Sales/Model/OrderPlugin.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @package Linkmobility\Notifications\Plugin\Sales\Model
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+declare(strict_types=1);
+
+namespace Linkmobility\Notifications\Plugin\Sales\Model;
+
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Model\Order;
+
+/**
+ * Plug-in for {@see \Magento\Sales\Model\Order}
+ *
+ * @package Linkmobility\Notifications\Plugin\Sales\Model
+ * @author Joseph Leedy <joseph@wagento.com>
+ */
+class OrderPlugin
+{
+    /**
+     * @var \Magento\Sales\Api\Data\OrderExtensionFactory
+     */
+    private $orderExtensionFactory;
+
+    public function __construct(OrderExtensionFactory $orderExtensionFactory)
+    {
+        $this->orderExtensionFactory = $orderExtensionFactory;
+    }
+
+    public function afterUnhold(Order $subject): Order
+    {
+        $orderExtension = $subject->getExtensionAttributes() ?? $this->orderExtensionFactory->create();
+
+        $orderExtension->setIsOrderHoldReleased(true);
+
+        $subject->setExtensionAttributes($orderExtension);
+
+        return $subject;
+    }
+}
---
+++ Test/Integration/Plugin/Sales/Model/OrderPluginTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @package Linkmobility\Notifications\Test\Integration\Plugin\Sales\Model
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+declare(strict_types=1);
+
+namespace Linkmobility\Notifications\Test\Integration\Plugin\Sales\Model;
+
+use Linkmobility\Notifications\Plugin\Sales\Model\OrderPlugin;
+use Magento\Framework\Interception\PluginList\PluginList;
+use Magento\Sales\Model\Order;
+use Magento\TestFramework\ObjectManager;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Order Plug-in Test
+ *
+ * @package Linkmobility\Notifications\Test\Integration\Plugin\Sales\Model
+ * @author Joseph Leedy <joseph@wagento.com>
+ */
+class OrderPluginTest extends TestCase
+{
+    /**
+     * @var \Magento\TestFramework\ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @magentoAppArea frontend
+     */
+    public function testPluginIsConfigured(): void
+    {
+        $pluginList = $this->objectManager->create(PluginList::class);
+        $plugins = $pluginList->get(Order::class, []);
+
+        $this->assertArrayHasKey('sms_notifications_add_order_extension_attribute', $plugins);
+        $this->assertSame(OrderPlugin::class, $plugins['sms_notifications_add_order_extension_attribute']['instance']);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoDataFixture orderDataFixtureProvider
+     */
+    public function testAfterUnholdSetsExtensionAttribute(): void
+    {
+        $order = $this->objectManager->create(Order::class);
+
+        $order->loadByIncrementId('100000001');
+        $order->hold();
+        $order->unhold();
+
+        $this->assertTrue($order->getExtensionAttributes()->getIsOrderHoldReleased());
+    }
+
+    public static function orderDataFixtureProvider(): void
+    {
+        require __DIR__ . '/../../../_files/order.php';
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->objectManager = ObjectManager::getInstance();
+    }
+}
