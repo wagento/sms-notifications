c5177cc5f456f23e382ef1ccd661bee0071de977
Joseph Leedy <joseph@wagento.com>
2019-02-15 23:45:27 -0600

    Add missing file header to module registration

--- registration.php
+++ registration.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'LinkMobility_SMSNotifications',
