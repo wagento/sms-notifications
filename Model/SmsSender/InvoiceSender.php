6675f98b787e01e0272354831d44c3923ad448c5
Joseph Leedy <joseph@wagento.com>
2019-03-19 16:06:41 -0500

    Create service for sending order invoiced SMS

---
+++ Model/SmsSender/InvoiceSender.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @package LinkMobility\SMSNotifications\Model
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+declare(strict_types=1);
+
+namespace LinkMobility\SMSNotifications\Model\SmsSender;
+
+use LinkMobility\SMSNotifications\Api\ConfigInterface;
+use LinkMobility\SMSNotifications\Api\SmsSubscriptionRepositoryInterface;
+use LinkMobility\SMSNotifications\Model\MessageService;
+use LinkMobility\SMSNotifications\Model\SmsSender;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Model\AbstractModel;
+use Magento\Sales\Api\Data\InvoiceExtensionFactory;
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Store\Api\StoreRepositoryInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Invoice SMS Sender
+ *
+ * @package LinkMobility\SMSNotifications\Model\SmsSender
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @api
+ */
+final class InvoiceSender extends SmsSender
+{
+    /**
+     * @var \Magento\Sales\Api\Data\InvoiceExtensionFactory
+     */
+    private $invoiceExtensionFactory;
+    /**
+     * @var \Magento\Sales\Api\Data\OrderExtensionFactory
+     */
+    private $orderExtensionFactory;
+
+    public function __construct(
+        LoggerInterface $logger,
+        StoreRepositoryInterface $storeRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        CustomerRepositoryInterface $customerRepository,
+        ConfigInterface $config,
+        SmsSubscriptionRepositoryInterface $subscriptionRepository,
+        MessageService $messageService,
+        InvoiceExtensionFactory $invoiceExtensionFactory,
+        OrderExtensionFactory $orderExtensionFactory
+    ) {
+        parent::__construct(
+            $logger,
+            $storeRepository,
+            $searchCriteriaBuilder,
+            $customerRepository,
+            $config,
+            $subscriptionRepository,
+            $messageService
+        );
+
+        $this->invoiceExtensionFactory = $invoiceExtensionFactory;
+        $this->orderExtensionFactory = $orderExtensionFactory;
+    }
+
+    /**
+     * @param \Magento\Framework\Model\AbstractModel|\Magento\Sales\Model\Order\Invoice $invoice
+     */
+    public function send(AbstractModel $invoice): bool
+    {
+        $storeId = (int)$invoice->getStoreId();
+        $websiteId = $this->getWebsiteIdByStoreId($storeId);
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $invoice->getOrder();
+        /** @var \Magento\Sales\Api\Data\OrderExtensionInterface $orderExtension */
+        $orderExtension = $order->getExtensionAttributes() ?? $this->orderExtensionFactory->create();
+        /** @var \Magento\Sales\Api\Data\InvoiceExtensionInterface $invoiceExtension */
+        $invoiceExtension = $invoice->getExtensionAttributes() ?? $this->invoiceExtensionFactory->create();
+
+        if (
+            !$this->isModuleEnabled($websiteId)
+            || (bool)$order->getCustomerIsGuest()
+            || $orderExtension->getIsSmsNotificationSent() === true
+            || $invoiceExtension->getIsSmsNotificationSent() === true
+        ) {
+            return false;
+        }
+
+        $customerId = (int)$order->getCustomerId();
+        $customer = $this->getCustomerById($customerId);
+
+        if ($customer === null) {
+            return false;
+        }
+
+        $messageRecipient = $this->getCustomerMobilePhoneNumber($customer);
+
+        if (
+            !in_array('order_invoiced', $this->getCustomerSmsSubscriptions($customerId), true)
+            || $messageRecipient === null
+        ) {
+            return false;
+        }
+
+        $this->messageService->setInvoice($invoice);
+
+        $messageTemplate = $this->config->getOrderInvoicedTemplate($storeId);
+        $messageSent = $this->messageService->sendMessage($messageTemplate, $messageRecipient, 'invoice');
+
+        $invoiceExtension->setIsSmsNotificationSent(true);
+
+        $invoice->setExtensionAttributes($invoiceExtension);
+
+        return $messageSent;
+    }
+}
---
+++ Test/Integration/Model/SmsSender/InvoiceSenderTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * LINK Mobility SMS Notifications
+ *
+ * Sends transactional SMS notifications through the LINK Mobility messaging
+ * service.
+ *
+ * @package LinkMobility\SMSNotifications\Test\Integration\Model\SmsSender
+ * @author Joseph Leedy <joseph@wagento.com>
+ * @author Yair García Torres <yair.garcia@wagento.com>
+ * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+declare(strict_types=1);
+
+namespace LinkMobility\SMSNotifications\Test\Integration\Model\SmsSender;
+
+use LinkMobility\SMSNotifications\Model\SmsSender\InvoiceSender;
+use LinkMobility\SMSNotifications\Test\Integration\SmsSenderTestCase;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Invoice;
+
+/**
+ * Invoice SMS Sender Test
+ *
+ * @package LinkMobility\SMSNotifications\Test\Integration\Model\SmsSender
+ * @author Joseph Leedy <joseph@wagento.com>
+ */
+class InvoiceSenderTest extends SmsSenderTestCase
+{
+    /**
+     * @magentoAppArea adminhtml
+     * @magentoDataFixture Magento/Customer/_files/customer.php
+     * @magentoDataFixture smsSubscriptionsFixtureProvider
+     * @magentoDataFixture smsMobileNumberFixtureProvider
+     * @magentoDataFixture customerInvoiceFixtureProvider
+     */
+    public function testSendInvoiceSmsForCustomer(): void
+    {
+        $configMock = clone $this->getConfigMock();
+
+        $configMock->expects($this->once())
+            ->method('getOrderInvoicedTemplate')
+            ->willReturn('Order invoiced');
+
+        $invoiceSender = $this->objectManager->create(
+            InvoiceSender::class,
+            [
+                'config' => $configMock,
+                'messageService' => $this->getMessageServiceMock()
+            ]
+        );
+
+        $this->assertTrue($invoiceSender->send($this->getInvoice()));
+    }
+
+    /**
+     * @magentoAppArea adminhtml
+     * @magentoDataFixture guestInvoiceFixtureProvider
+     */
+    public function testSendInvoiceSmsForGuest(): void
+    {
+        $invoiceSender = $this->objectManager->create(
+            InvoiceSender::class,
+            [
+                'config' => $this->getConfigMock(),
+                'messageService' => $this->getMessageServiceMock()
+            ]
+        );
+
+        $this->assertFalse($invoiceSender->send($this->getInvoice()));
+    }
+
+    public static function customerInvoiceFixtureProvider(): void
+    {
+        require __DIR__ . '/../../_files/invoice.php';
+    }
+
+    public static function guestInvoiceFixtureProvider(): void
+    {
+        require __DIR__ . '/../../_files/invoice_guest.php';
+    }
+
+    private function getInvoice(): Invoice
+    {
+        $order = $this->objectManager->create(Order::class)->loadByIncrementId('100000001');
+
+        return $order->getInvoiceCollection()->getFirstItem();
+    }
+}
