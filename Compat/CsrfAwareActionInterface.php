28cf84216bb88170da8e2a6c9a373c43f5323a17
Joseph Leedy <joseph@wagento.com>
2019-03-22 15:53:43 -0500

    Fix incorrect package annotation in file headers

--- Compat/CsrfAwareActionInterface.php
+++ Compat/CsrfAwareActionInterface.php
@@ -5,7 +5,7 @@
  * Sends transactional SMS notifications through the LINK Mobility messaging
  * service.
  *
- * @package LinkMobility\SMSNotifications\Controller\SmsNotifications
+ * @package LinkMobility\SMSNotifications\Compat
  * @author Joseph Leedy <joseph@wagento.com>
  * @author Yair García Torres <yair.garcia@wagento.com>
  * @copyright Copyright (c) LINK Mobility (https://www.linkmobility.com/)
