2abeb2eed96dbbc1891a27f24ea5341d004e2427
Joseph Leedy <joseph@wagento.com>
2019-03-28 08:15:59 -0500

    Refactor logic for selecting all SMS type preferences

--- view/frontend/web/js/view/sms-subscription-preferences.js
+++ view/frontend/web/js/view/sms-subscription-preferences.js
@@ -12,17 +12,22 @@
 
 define([
     'jquery',
+    'ko',
     'uiComponent',
     'LinkMobility_SMSNotifications/js/model/sms-subscription-preferences-modal',
     'smsNotifications'
-], function ($, Component, subscriptionPreferencesModal, smsNotifications) {
+], function ($, ko, Component, subscriptionPreferencesModal, smsNotifications) {
     'use strict';
 
     return Component.extend({
         defaults: {
             template: 'LinkMobility_SMSNotifications/sms-subscription-preferences',
             groupedSmsTypes: {},
-            modalTitle: null
+            selectedSmsTypes: [],
+            modalTitle: null,
+            tracks: {
+                selectedSmsTypes: true
+            }
         },
         modalTrigger: null,
         initObservable: function () {
@@ -30,34 +35,32 @@ define([
 
             subscriptionPreferencesModal.open.subscribe(this.showModal);
 
+            const smsTypeCodes = this.groupedSmsTypes.map(groupedSmsType => groupedSmsType.smsTypes)
+                .reduce(
+                    (accumulator, currentValue) => [...accumulator, ...currentValue.map(smsType => smsType.code)],
+                    []
+                );
+
+            this.selectAllSmsTypes = ko.pureComputed({
+                read: () => this.selectedSmsTypes.length === smsTypeCodes.length,
+                write: value => { this.selectedSmsTypes = value ? smsTypeCodes.slice(0) : []; },
+                owner: this
+            });
+
+            if (this.selectedSmsTypes.length === 0) {
+                this.selectAllSmsTypes(true);
+            }
+
             return this;
         },
         initModal: function(element) {
-            subscriptionPreferencesModal.createModal(element, this.modalTitle, this.hideModal);
+            subscriptionPreferencesModal.createModal(element, this.modalTitle, this.hideModal.bind(this));
         },
         showModal: function () {
             subscriptionPreferencesModal.showModal();
         },
-        hideModal: function (context) {
-            smsNotifications.selectedSmsTypes.removeAll();
-
-            $(context.element).find('.sms-type-checkbox:checked').each(function () {
-                smsNotifications.selectedSmsTypes.push($(this).val());
-            });
-        },
-        selectAll: function () {
-            $('.sms-type-checkbox').each(function () {
-                $(this).prop('checked', $('#sms-type-select-all').prop('checked'));
-            });
-
-            return true;
-        },
-        toggleSelectAll: function () {
-            const $checkboxes = $('.sms-type-checkbox');
-
-            $('#sms-type-select-all').prop('checked', $checkboxes.filter(':checked').length === $checkboxes.length);
-
-            return true;
+        hideModal: function () {
+            smsNotifications.selectedSmsTypes(this.selectedSmsTypes);
         }
     });
 });
