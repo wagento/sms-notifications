84fd2135c2ae93f0c814219699691ed55be03223
Joseph Leedy <joseph@wagento.com>
2019-08-25 22:21:14 -0500

    Render 404 error page if extension is disabled

--- Controller/SmsNotifications/Manage.php
+++ Controller/SmsNotifications/Manage.php
@@ -17,6 +17,11 @@ declare(strict_types=1);
 namespace Wagento\SMSNotifications\Controller\SmsNotifications;
 
 use Magento\Customer\Controller\AbstractAccount;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\NotFoundException;
+use Magento\Store\Model\StoreManagerInterface;
+use Wagento\SMSNotifications\Model\Config;
 
 /**
  * Manage SMS Subscriptions Controller
@@ -28,10 +33,37 @@ use Magento\Customer\Controller\AbstractAccount;
 class Manage extends AbstractAccount
 {
     /**
+     * @var \Magento\Store\Api\StoreManagementInterface
+     */
+    private $storeManager;
+    /**
+     * @var \Wagento\SMSNotifications\Model\Config
+     */
+    private $config;
+
+    public function __construct(Context $context, StoreManagerInterface $storeManager, Config $config)
+    {
+        parent::__construct($context);
+
+        $this->storeManager = $storeManager;
+        $this->config = $config;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function execute()
     {
+        try {
+            $websiteId = (int)$this->storeManager->getStore()->getWebsiteId();
+        } catch (NoSuchEntityException $e) {
+            $websiteId = null;
+        }
+
+        if (!$this->config->isEnabled($websiteId)) {
+            throw new NotFoundException(__('SMS Notifications is not enabled.'));
+        }
+
         $this->_view->loadLayout();
         $this->_view->renderLayout();
     }
